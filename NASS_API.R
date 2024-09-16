17bc6fff4dcd39270c9040a82d48ea7166ce2a31
andrew-s-thompson <thethompson3@icloud.com>
2024-09-16 14:46:31 -0500

    Delete Duplicated Data Files, Combine (successful) county data from each census year. 77, 02, 07, 12, 17, 22.

--- NASS_API.R
+++ NASS_API.R
@@ -30,10 +30,31 @@ params <- list(
 }
 
 write.csv(df, "DATA_FILES/1977_county_crops.csv", row.names = FALSE)
+df
 
 # Write them from csv to RData for storage purposes
 data <- read.csv("DATA_FILES/2002_county_crops.csv")
 save(data, file = "DATA_FILES/2002_county_crops.RData")
 
 
+
+
+
+files <- c("DATA_FILES/2002_county_crops.RData", "DATA_FILES/2007_county_crops.RData", "DATA_FILES/2012_county_crops.RData", "DATA_FILES/2017_county_crops.RData", "DATA_FILES/2022_county_crops.RData")
+
+# Initialize an empty list to store the data frames
+data_list <- list()
+
+# Load each RData file and extract the single dataframe
+for (file in files) {
+  before_load <- ls()
+  print(ls())
+  load(file)  # Load the RData file
+  after_load <- setdiff(ls(), before_load)
+  data_list[[file]] <- get(after_load[1])
+}
+
+combined_data <- do.call(rbind, data_list)
+
+save(combined_data, file = "DATA_FILES/county_crop_combined.RData")
 
