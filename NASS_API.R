413fb8c947773455a684f8f9ae566f779e76157f
andrew-s-thompson <thethompson3@icloud.com>
2024-09-16 11:41:42 -0500

    Crops Data For County File

--- NASS_API.R
+++ NASS_API.R
@@ -9,8 +9,27 @@ nassqs_auth(key = "E815374B-D025-33BA-AF2A-990B8657C49B")
 # Parameters to query on and data call
 # look at the link: https://cran.r-project.org/web/packages/rnassqs/vignettes/rnassqs.html
 
-params <- list(commodity_desc = "CORN", year = c(2000), state_alpha = c("IN","VA"))
+params <- list(sector = "CROPS", year = c(2002), state_alpha = c("IN"), )
 d <- nassqs(params)
+
+library(rnassqs)
+
+df = data.frame(matrix(nrow = 0, ncol = length(colnames(data)))) 
+colnames(df) = colnames(data)
+
+for(state in c("IN", "OH", "IL", "MI", "KY", "MO")){
+# Set up the parameters
+params <- list(
+  sector_desc = "CROPS",
+  year = 2002,
+  state_alpha = state, 
+  agg_level_desc = "COUNTY")
+  data <- nassqs(params)
+  df = rbind(df, data)
+
+}
+
+write.csv(df, "DATA_FILES/2002_county_crops", row.names = FALSE)
 
 
 
