7f844726ab5b4215d5d528845a0e200635d1a8b7
andrew-s-thompson <thethompson3@icloud.com>
2024-11-18 17:37:20 -0600

    Attempt to Remove NA value from YIELD

--- New_states.R
+++ New_states.R
@@ -186,7 +186,7 @@ wide_data <- complete_data %>%
   ) %>% 
   arrange(YEAR)
 wide_data
-wide_data$YIELD_ = ifelse(wide_data$PRODUCTION_BU == 0 | wide_data$`AREA HARVESTED_ACRES` == 0, 0, wide_data$PRODUCTION_BU/wide_data$`AREA HARVESTED_ACRES`)
+wide_data$`YIELD_BU PER ACRE` = ifelse(wide_data$PRODUCTION_BU == 0 | wide_data$`AREA HARVESTED_ACRES` == 0, 0, wide_data$PRODUCTION_BU/wide_data$`AREA HARVESTED_ACRES`)
 
 # Create county_state column in the second data
 corn_county_geometry <- corn_county_geometry %>%
@@ -525,7 +525,7 @@ wide_data <- complete_data %>%
   ) %>% 
   arrange(YEAR)
 
-wide_data$YIELD_ = ifelse(wide_data$PRODUCTION_BU == 0 | wide_data$`AREA HARVESTED_ACRES` == 0, 0, wide_data$PRODUCTION_BU/wide_data$`AREA HARVESTED_ACRES`)
+wide_data$`YIELD_BU PER ACRE` = ifelse(wide_data$PRODUCTION_BU == 0 | wide_data$`AREA HARVESTED_ACRES` == 0, 0, wide_data$PRODUCTION_BU/wide_data$`AREA HARVESTED_ACRES`)
 
 # Create county_state column in the second data
 soybeans_county_geometry <- soybeans_county_geometry %>%
@@ -533,6 +533,8 @@ soybeans_county_geometry <- soybeans_county_geometry %>%
 
 soybeans_census_final <- wide_data %>%
   left_join(soybeans_county_geometry[, c("county_state", "geometry")], by = "county_state")
+
+soybeans_census_final
 ###################################################
 # survey:
 load("soybeans_survey.rda")
@@ -731,7 +733,7 @@ wide_data <- complete_data %>%
   ) %>% 
   arrange(YEAR)
 
-wide_data$YIELD__ = ifelse(wide_data$PRODUCTION_CWT == 0 | wide_data$`AREA HARVESTED_ACRES` == 0, 0, wide_data$PRODUCTION_CWT/wide_data$`AREA HARVESTED_ACRES`)
+wide_data$`YIELD_BU PER ACRE` = ifelse(wide_data$PRODUCTION_CWT == 0 | wide_data$`AREA HARVESTED_ACRES` == 0, 0, wide_data$PRODUCTION_CWT/wide_data$`AREA HARVESTED_ACRES`)
 
 
 
@@ -935,7 +937,7 @@ wide_data <- complete_data %>%
   ) %>% 
   arrange(YEAR)
 
-wide_data$YIELD_ = ifelse(wide_data$PRODUCTION_BU == 0 | wide_data$`AREA HARVESTED_ACRES` == 0, 0, wide_data$PRODUCTION_BU/wide_data$`AREA HARVESTED_ACRES`)
+wide_data$`YIELD_BU PER ACRE` = ifelse(wide_data$PRODUCTION_BU == 0 | wide_data$`AREA HARVESTED_ACRES` == 0, 0, wide_data$PRODUCTION_BU/wide_data$`AREA HARVESTED_ACRES`)
 
 # Create county_state column in the second data
 wheat_county_geometry <- wheat_county_geometry %>%
